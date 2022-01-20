1bec35b5bd51d207b96eb94fb96ef98318a0b463
Geca13 <karafiloski84@gmail.com>
2022-01-20 22:58:04 +0100

    first commit

---
+++ src/main/java/com/example/guru/GuruPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guru/GuruPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
